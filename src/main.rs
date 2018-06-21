5a916d4471fb1fb1fdb1d32c02d525789e7d185d
mbaris <mehmetbaris.kalkar@gmail.com>
2018-06-21 23:32:52 +0300

    Use variables with types

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
 
-    let x = 42;
+    let fourty_two:i8 = 42;
+    let degrees:f32 = 25.67;
+    let is_true:bool=false;
 
-    println!("The answer to the ultimate question of life, the universe and everything is {}",x);
+    println!("The answer to the ultimate question of life, the universe and everything is {}",fourty_two);
+    println!("Today the weather is {} degress",degrees);
+    println!("boolean {}",is_true)
 }
