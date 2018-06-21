6edf021cdbefc8efa6c81e1fe8e434498a720d53
mbaris <mehmetbaris.kalkar@gmail.com>
2018-06-21 23:40:53 +0300

    Use if else statements

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
+    let n = 10;
+    let m = 39;
 
-    let fourty_two:i8 = 42;
-    let degrees:f32 = 25.67;
-    let is_true:bool=false;
-
-    println!("The answer to the ultimate question of life, the universe and everything is {}",fourty_two);
-    println!("Today the weather is {} degress",degrees);
-    println!("boolean {}",is_true)
+    if n > m {
+        println!("{} is greater than {}",n,m);
+    } else if n == 39 {
+        println!("{} is equal to {}",n,m);
+    } else {
+        println!("{} is less than {}",n,m);
+    }
 }
